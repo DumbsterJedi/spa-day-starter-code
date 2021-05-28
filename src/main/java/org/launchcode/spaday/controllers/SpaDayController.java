0afa862ba944b03e97ec16aa1ba9f86c6849e5b4
Lucas <lucasr.reehle@gmail.com>
2021-05-27 21:13:22 -0400

    finished without bonus

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -25,26 +25,26 @@ public class SpaDayController {
     }
 
     @GetMapping(value="")
-    @ResponseBody
+//    @ResponseBody
     public String customerForm () {
-        String html = "<form method = 'post'>" +
-                "Name: <br>" +
-                "<input type = 'text' name = 'name'>" +
-                "<br>Skin type: <br>" +
-                "<select name = 'skintype'>" +
-                "<option value = 'oily'>Oily</option>" +
-                "<option value = 'combination'>Combination</option>" +
-                "<option value = 'normal'>Normal</option>" +
-                "<option value = 'dry'>Dry</option>" +
-                "</select><br>" +
-                "Manicure or Pedicure? <br>" +
-                "<select name = 'manipedi'>" +
-                "<option value = 'manicure'>Manicure</option>" +
-                "<option value = 'pedicure'>Pedicure</option>" +
-                "</select><br>" +
-                "<input type = 'submit' value = 'Submit'>" +
-                "</form>";
-        return html;
+//        String html = "<form method = 'post'>" +
+//                "Name: <br>" +
+//                "<input type = 'text' name = 'name'>" +
+//                "<br>Skin type: <br>" +
+//                "<select name = 'skintype'>" +
+//                "<option value = 'oily'>Oily</option>" +
+//                "<option value = 'Combination'>Combination</option>" +
+//                "<option value = 'Normal'>Normal</option>" +
+//                "<option value = 'Dry'>Dry</option>" +
+//                "</select><br>" +
+//                "Manicure or Pedicure? <br>" +
+//                "<select name = 'manipedi'>" +
+//                "<option value = 'Manicure'>Manicure</option>" +
+//                "<option value = 'Pedicure'>Pedicure</option>" +
+//                "</select><br>" +
+//                "<input type = 'submit' value = 'Submit'>" +
+//                "</form>";
+        return "spaMenu";
     }
 
     @PostMapping(value="")
@@ -62,6 +62,10 @@ public class SpaDayController {
                 appropriateFacials.add(facials.get(i));
             }
         }
+        model.addAttribute("name", name);
+        model.addAttribute("skintype", skintype);
+        model.addAttribute("manipedi", manipedi);
+        model.addAttribute("appropriateFacials", appropriateFacials);
 
         return "menu";
     }
